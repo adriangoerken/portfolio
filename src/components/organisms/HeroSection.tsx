a55ed84459597dd0fd1e8af826b9c1c0e94996ab
Adrian Goerken <adrian.goerken@proton.me>
2025-01-29 11:45:00 +0100

    Remove left-over comments in HeroSection

--- src/components/organisms/HeroSection.tsx
+++ src/components/organisms/HeroSection.tsx
@@ -1,5 +1,5 @@
 import Button from '../atoms/Button';
-import { Trans, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 type HeroSectionProps = {
 	sectionHeroRef: React.RefObject<HTMLElement>;
@@ -12,47 +12,7 @@ A professional photo or a creative illustration of yourself
 A brief, impactful introduction (e.g., “Hi, I'm [Your Name], a passionate web developer specializing in creating dynamic and responsive websites.”)
 Call to Action (e.g., “Explore My Work” button)
 */
-// const HeroSection = ({
-// 	sectionHeroRef,
-// 	sectionProjectsRef,
-// 	scrollToSection,
-// }: HeroSectionProps) => {
-// 	const [t] = useTranslation('global');
 
-// 	return (
-// 		<section
-// 			ref={sectionHeroRef}
-// 			className="flex flex-col md:flex-row justify-around"
-// 		>
-// 			{/* Left Column */}
-// 			<div className="flex flex-col items-center md:items-start gap-6 md:self-center md:w-1/2">
-// 				<h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight text-center md:text-start">
-// 					<Trans i18nKey="HeroSection.title">
-// 						Nice to meet you! I'm
-// 						<span className="text-blue-700"> Adrian.</span>
-// 					</Trans>
-// 				</h1>
-// 				<div className="flex flex-col gap-3">
-// 					<p>{t('HeroSection.messageOne')}</p>
-// 					<p>{t('HeroSection.messageTwo')}</p>
-// 					<p className="italic">{t('HeroSection.messageThree')}</p>
-// 				</div>
-// 				<Button onClick={() => scrollToSection(sectionProjectsRef)}>
-// 					{t('HeroSection.btnCTA')}
-// 				</Button>
-// 			</div>
-
-// 			{/* Right Column */}
-// 			<div className="md:s-1/2 flex justify-center items-center mt-8 md:mt-0">
-// 				<img
-// 					src="src/assets/images/portrait.jpg"
-// 					alt={t('HeroSection.imgAlt')}
-// 					className="h-80 lg:h-96 w-80 lg:w-96 object-cover rounded-full object-custom-top"
-// 				/>
-// 			</div>
-// 		</section>
-// 	);
-// };
 const HeroSection = ({
 	sectionHeroRef,
 	sectionProjectsRef,
