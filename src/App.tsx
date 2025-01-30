31f9efcd37d28f2a5128e092a8f723c85b8566e0
Adrian Goerken <adrian.goerken@proton.me>
2025-01-30 18:49:20 +0100

    Add NotFoundPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import IndexPage from './pages/IndexPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
 import MainLayout from './layouts/MainLayout';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
 	return (
@@ -15,6 +16,8 @@ function App() {
 						path="privacy-policy"
 						element={<PrivacyPolicyPage />}
 					/>
+					{/* 404 page */}
+					<Route path="*" element={<NotFoundPage />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import Container from '../components/atoms/Container';
+import { useTranslation } from 'react-i18next';
+
+const NotFoundPage = () => {
+	const [t] = useTranslation('global');
+
+	return (
+		<Container className="flex flex-grow justify-center">
+			<section className="text-center flex flex-col justify-center items center h96">
+				<h1 className="text-6xl font-bold mb-4">
+					{t('NotFoundPage.message')}
+				</h1>
+				<Link to="/" className="hover:underline">
+					{t('NotFoundPage.btnBack')}
+				</Link>
+			</section>
+		</Container>
+	);
+};
+
+export default NotFoundPage;
